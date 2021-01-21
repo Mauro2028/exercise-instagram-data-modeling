dd05aaf71eab84fe52623445c95d739f52bd9176
Mauro2028 <mauriciolizcano25531@gmail.com>
2021-01-21 15:49:01 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,64 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email=Column(String(250),nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name=Column(String(250),nullable=False)
+    password=Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    post=relationship("Post", backref="user")
+    comment=relationship("Comment",backref="user")
+    post_like=relationship("Post_Like",backref="user")
+    def to_dict(self):
+        return{}
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id=Column(String(250),ForeignKey("user.id"))
+    image_url=Column(String(250),nullable=False)
+    date_published=Column(String(250),nullable=False)
+    content=Column(String(250),nullable=False)
+    longitude=Column(String(250),nullable=False)
+    comments=Column(String(250),nullable=False)
 
     def to_dict(self):
         return {}
+
+
+class PostLike(Base):
+    __tablename__="postlike"
+    id=Column(Integer,primary_key=True)
+    post_id=Column(Integer,ForeignKey("post.id"))
+    user_id=Column(Integer,ForeignKey("user.id"))
+
+    def to_dict(self):
+        return {}
+
+
+class Comment(Base):
+    __tablename__="comment"
+    id=Column(Integer,primary_key=True)
+    post_id=Column(Integer,ForeignKey("post.id"))
+    post=Column(String(250),nullable=False)
+    user_id=Column(Integer)
+    likes=relationship("CommentLike",backref="comment")
+    
+    def to_dict(self):
+       return {}
+
+class CommentLike(Base):
+    __tablename__="comment_like"
+    id= Column(Integer,primary_key=True)
+    comment_id=Column(Integer,ForeignKey("comment.id"))
+    user_id=Column(Integer,ForeignKey("user.id"))
+
+    def to_dict(self):
+        return{}
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
